a1e98a8677ed6dc36b7024b536f99203a71b82a8
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-11-23 21:15:59 -0600

    binding a list of objects.

--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GreetingComponent implements OnInit {
 
+  // 'names' is now a string of objects
   names: [{name: string}] = [{name: 'Kyra'}, {name: 'Mud'}, {name: 'Kierra'}, {name: 'Owen'}, {name: 'Maggie'}];
 
   constructor() { }
