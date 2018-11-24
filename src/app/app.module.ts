af3600104a5253516d9c3b44f239c299aac789cb
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-11-23 20:43:32 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+// this is a root module inside of applications
+// used to define further functionalities of apps
+// modules are used to define a particular
+// set of features
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
+// the app component belongs to this module
+// defines an NgModule
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
